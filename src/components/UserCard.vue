fffe6d3c686218b764522ecbe3d4b8e209f3f8ab
Alexandre Oliveira <alexandrebo@gmail.com>
2025-01-28 00:17:31 +0100

    implement user list with dynamic loading and card display

---
+++ src/components/UserCard.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface User {
+  picture: {
+    thumbnail: string
+  }
+  name: {
+    first: string
+    last: string
+  }
+  email: string
+}
+
+defineProps<{
+  userItem: User
+}>()
+</script>
+
+<template>
+  <div class="UserCard">
+    <img :src="userItem.picture.thumbnail" alt="User Thumbnail" />
+    <div class="userCardInfo">
+      <p>{{ userItem.name.first }} {{ userItem.name.last }}</p>
+      <p class="email">{{ userItem.email }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.UserCard {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border: 1px solid #ccc;
+  margin-bottom: 10px;
+  border-radius: 8px;
+  background-color: #fff;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.UserCard:hover {
+  background-color: #f0f0f0;
+}
+
+.UserCard img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import Loader from './Loader.vue'
+import UserCard from './UserCard.vue'
+
+interface User {
+  login: {
+    uuid: string
+  }
+  [key: string]: any // To allow additional user properties
+}
+
+defineProps<{
+  userList: User[]
+  loading: boolean
+}>()
+</script>
+
+<template>
+  <div class="UserList">
+    <UserCard
+      v-for="user in userList"
+      :key="user.login.uuid"
+      :userItem="user"
+    />
+    <p class="msg" v-if="userList.length === 0 && !loading">The list is currently empty</p>
+    <Loader v-if="loading" />
+  </div>
+</template>
+
+<style scoped>
+.UserList {
+  width: 600px;
+  height: 600px;
+  overflow-y: auto;
+  position: relative;
+  background-color: teal;
+  border-top-left-radius: 5px;
+}
+
+.msg {
+  font-family: 'Lucida Sans', Geneva, Verdana, sans-serif;
+  position: absolute;
+  top: 40%;
+  color: white;
+  left: 37%;
+}
+
+@media (max-width: 800px) {
+  .UserList {
+    width: 100%;
+  }
+
+  .msg {
+    left: 30%;
+  }
+}
+</style>
