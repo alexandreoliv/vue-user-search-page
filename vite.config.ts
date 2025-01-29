3a0c445171e72faf4ac7b462a8f702dca511bb4c
Alexandre Oliveira <alexandrebo@gmail.com>
2025-01-29 17:18:01 +0100

    remove unnecessary resources

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    allowedHosts: ['57a2-95-88-64-22.ngrok-free.app'], // Add the ngrok domain here
   },
 })
